404f51b2af8a046be94993082a27fb22b4706db3
Fatheali Landage <fatealilandage@gmail.com>
2026-01-24 16:51:24 +0530

    Add JWT authentication middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const header = req.header("Authorization");
+
+  if (!header)
+    return res.status(401).json({ message: "No token provided" });
+
+  try {
+    const token = header.replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
