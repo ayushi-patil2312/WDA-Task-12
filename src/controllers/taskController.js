6f5732ca8127800f94f5d1a675ed750429d3f1fb
Fatheali Landage <fatealilandage@gmail.com>
2026-01-24 16:54:04 +0530

    Create taskController.js

---
+++ src/controllers/taskController.js
@@ -0,0 +1,44 @@
+const Task = require("../models/Task");
+
+exports.createTask = async (req, res) => {
+  const task = await Task.create({
+    ...req.body,
+    user: req.userId
+  });
+  res.status(201).json(task);
+};
+
+exports.getTasks = async (req, res) => {
+  const { page = 1, limit = 5, completed } = req.query;
+
+  const filter = { user: req.userId };
+  if (completed !== undefined) {
+    filter.completed = completed === "true";
+  }
+
+  const tasks = await Task.find(filter)
+    .limit(limit)
+    .skip((page - 1) * limit)
+    .sort({ createdAt: -1 });
+
+  res.json(tasks);
+};
+
+exports.updateTask = async (req, res) => {
+  const task = await Task.findOneAndUpdate(
+    { _id: req.params.id, user: req.userId },
+    req.body,
+    { new: true }
+  );
+
+  if (!task) return res.status(404).json({ message: "Task not found" });
+  res.json(task);
+};
+
+exports.deleteTask = async (req, res) => {
+  await Task.findOneAndDelete({
+    _id: req.params.id,
+    user: req.userId
+  });
+  res.json({ message: "Task deleted" });
+};
